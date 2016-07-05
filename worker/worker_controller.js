2b5a19bf4acb530a4fb9da17ffd5b30addcf2580
Felix <felix2feng@gmail.com>
2016-07-05 16:52:05 -0700

    Made some minor changes

--- worker/worker_controller.js
+++ worker/worker_controller.js
@@ -8,60 +8,26 @@ const Promise = require('bluebird');
 let jobsCompleted = 0;
 
 const handleJob = (jobs, masterUrl) => {
-  /* Jobs input
-  [
-    {
-      scenarioID: req.body.scenarioID,
-      scenario: req.body.scenarioName,
-      user: req.body.id_user,
-      targetURL: req.body.targetURL,
-      script: req.body.script,
-    },
-    ...
-  ]
-  */
-  const requestUrl = masterUrl + '/api/requestJob';
-  const resultUrl = masterUrl + '/api/complete';
-
-  console.log('Got some work from the server', jobs);
+  const requestUrl = `${masterUrl}/api/requestJob`;
   const results = [];
 
-  Promise.mapSeries(jobs, (job => {
-    console.log('run that job');
-    return scenariorunner.run(job.targetUrl, job.script)
-    .then((runresults) => {
-    /*
-    runresults: {
-      scenarioTime: timeToRunScenarioInMilliseconds,
-      transactionTimes: [
-        {path, statusCode, elapsedTime, dataSizeInBytes, 'GET'},
-      ]
-    }
-    */
-      console.log('runresults', runresults);
-      // For each job result, save actions to the actions database
-      const actionsResults = runresults.transactionTimes;
-      helpers.saveActionResultsToDB(actionsResults, job);
+  Promise.mapSeries(jobs, (job =>
+    scenariorunner.run(job.targetUrl, job.script)
+      .then((runresults) => {
+        // For each job result, save actions to the actions database
+        const actionsResults = runresults.transactionTimes;
+        helpers.saveActionResultsToDB(actionsResults, job);
 
-      // Save to spawn database
-      helpers.saveSpawnsToDB(runresults, job);
+        // Save to spawn database
+        helpers.saveSpawnsToDB(runresults, job);
 
-      // Add to results
-      results.push(runresults);
-      jobsCompleted++;
-    })
-    .catch((e) => console.log('err:', e));
-  }))
+        // Add to results
+        results.push(runresults);
+        jobsCompleted++;
+      })
+      .catch((e) => console.log('err:', e))
+  ))
   .then(() => {
-    console.log('after job execution');
-
-    // Post results to master server
-    request.post({
-      url: resultUrl,
-      json: true,
-      body: results,
-    });
-
     const responseFromMasterCallback = (error, response, body) => {
       if (error) {
         console.error(error);
